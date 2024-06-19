197d684160b9aab220e6425802ab637e9e1ab985
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-19 12:47:40 +0100

    Commit

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -7,22 +7,29 @@ const CurrencyConverter = () => {
   const [fromCurr, setFromCurr] = useState("EUR");
   const [toCurr, setToCurr] = useState("USD");
   const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetCurrency = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`
         );
         const data = await res.json();
         setConverted(data.rates[toCurr]);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
 
+    if (fromCurr === toCurr) {
+      setConverted(amount);
+    }
+
     fetCurrency();
-  }, []);
+  }, [amount, fromCurr, toCurr]);
 
   return (
     <div className="currency">
@@ -32,26 +39,31 @@ const CurrencyConverter = () => {
             type="text"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
+            disabled={isLoading}
           />
           <select
             value={fromCurr}
             onChange={(e) => setFromCurr(e.target.value)}
           >
-            <option value="USD"></option>
-            <option value="EUR"></option>
-            <option value="CAD"></option>
-            <option value="INR"></option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="INR">INR</option>
+            <option value="NGN">NGN</option>
           </select>
           <select value={toCurr} onChange={(e) => setToCurr(e.target.value)}>
-            <option value="USD"></option>
-            <option value="EUR"></option>
-            <option value="CAD"></option>
-            <option value="INR"></option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="INR">INR</option>
+            <option value="NGN">NGN</option>
           </select>
         </form>
       </div>
 
-      <p>{converted}</p>
+      <p>
+        {converted} {toCurr}
+      </p>
     </div>
   );
 };
